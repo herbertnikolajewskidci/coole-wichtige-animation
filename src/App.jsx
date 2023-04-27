501279cacaccdfff4e809ba1eeadb74d2873ee9d
Herbert Nikolajewski <herbert.nikolajewksi@digitalcareerinstitute.org>
2023-04-27 13:22:09 +0200

    !

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1>Vite + React!!!!!!!!!</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 10)}>
                     count is {count}
